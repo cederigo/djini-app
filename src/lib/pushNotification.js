7b8be97263bb61e13934b7c02babdc185c69cbbd
Cédric Reginster <cedric.reginster@gmail.com>
2016-09-22 14:52:35 +0200

    Add 'task' push notifications

--- src/lib/pushNotification.js
+++ src/lib/pushNotification.js
@@ -38,22 +38,30 @@ export function getLocalNotifications(notes) {
 
   let result = []
   List(notes)
-    .filter((note) => note.dueDate && note.type === 'reminder' && !note.done)
+    .filter((note) => note.dueDate && !note.done)
     .groupBy(groupNote)
     .forEach((entries) => {
       entries = entries.toJS()
       // All entries share the same type. See `groupBy` above.
-      const dueDate = entries[0].dueDate
+      const note = entries[0]
+      const type = note.type
+      const dueDate = note.dueDate
       const names = entries.map((e) => e.contact.name)
       // 1 Week before @ 9.00h
       result.push({
         fireDate: moment(dueDate).subtract(7, 'days').hours(9).minute(0).valueOf(),
-        alertBody: `${listString(names)} ${quantityString(names.length, 'hat', 'haben')} am ${moment(dueDate).format('Do MMMM')} Geburtstag`
+        alertBody: 
+          type === 'reminder' ?
+            `${listString(names)} ${quantityString(names.length, 'hat', 'haben')} am ${moment(dueDate).format('Do MMMM')} Geburtstag`
+            : `Du hast noch eine Woche um das Geschenk "${note.wish.title}" zu organisieren. `
       })
       // The same day @ 9.00h
       result.push({
         fireDate: moment(dueDate).hours(9).minute(0).valueOf(),
-        alertBody: `${listString(names)} ${quantityString(names.length, 'hat', 'haben')} heute Geburtstag`
+        alertBody: 
+          type === 'reminder' ?
+            `${listString(names)} ${quantityString(names.length, 'hat', 'haben')} heute Geburtstag`
+            : `Hast du das Geschenk "${note.wish.title}" organisiert? Der Anlass findet heute statt.`
       })
     })
 
