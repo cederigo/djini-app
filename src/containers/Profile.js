a116dfe377f2e4a51ea0665ac90f329a488bb96f
Cédric Reginster <cedric.reginster@soom-it.ch>
2018-02-28 10:52:29 +0100

    feat: show logout button in dev mode

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,16 +1,18 @@
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { StyleSheet, View} from 'react-native';
-import {Actions} from 'react-native-router-flux'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Actions } from 'react-native-router-flux'
 
-import {editProfile, updateProfile, cancelEditProfile, onFieldChange} from '../actions/profile'
-import {version, showVersionNumber} from '../lib/config'
+import { editProfile, updateProfile, cancelEditProfile, onFieldChange } from '../actions/profile'
+import { logout } from '../actions/authActions'
+import { version, showVersionNumber, showLogoutButton } from '../lib/config'
 
-import {AppBar, ActionButton} from '../components/AppBar'
+import { AppBar, ActionButton } from '../components/AppBar'
 import ProfileField from '../components/ProfileField'
 import DjiniText from '../components/DjiniText'
 import Tabs from '../components/Tabs'
+import DjiniButton from '../components/DjiniButton'
 
 class Profile extends Component {
   static propTypes = {
@@ -22,44 +24,93 @@ class Profile extends Component {
   }
 
   render() {
-    const {dispatch, user, edit, isValid, isFetching, fields} = this.props
+    const { dispatch, user, edit, isValid, isFetching, fields } = this.props
     const editable = edit && !isFetching
-    return(
+    return (
       <View style={styles.container}>
         <View style={styles.appBar}>
-          <AppBar textStyle="dark" title="Mein Profil" showBackButton={edit} backButtonText="Abbrechen" onBack={() => dispatch(cancelEditProfile())}>
-            {edit ? 
-              <ActionButton textStyle="dark" text="Fertig" disabled={!isValid || isFetching} onPress={() => dispatch(updateProfile(fields, 'profile-edit'))}/>  
-              : <ActionButton textStyle="dark" text="Bearbeiten" onPress={() => dispatch(editProfile(user))}/>
-            }
+          <AppBar
+            textStyle="dark"
+            title="Mein Profil"
+            showBackButton={edit}
+            backButtonText="Abbrechen"
+            onBack={() => dispatch(cancelEditProfile())}
+          >
+            {edit ? (
+              <ActionButton
+                textStyle="dark"
+                text="Fertig"
+                disabled={!isValid || isFetching}
+                onPress={() => dispatch(updateProfile(fields, 'profile-edit'))}
+              />
+            ) : (
+              <ActionButton
+                textStyle="dark"
+                text="Bearbeiten"
+                onPress={() => dispatch(editProfile(user))}
+              />
+            )}
           </AppBar>
         </View>
         <Tabs selected="profile">
-          <DjiniText style={styles.tabText} name="profile">Details</DjiniText>
-          <DjiniText style={styles.tabText} name="more" onSelect={() => Actions.more()}>Mehr</DjiniText>
+          <DjiniText style={styles.tabText} name="profile">
+            Details
+          </DjiniText>
+          <DjiniText style={styles.tabText} name="more" onSelect={() => Actions.more()}>
+            Mehr
+          </DjiniText>
         </Tabs>
         <View style={styles.fields}>
-          <ProfileField iconName="person" value={edit ? fields.name : user.name}
-              editable={editable} onChangeText={(text) => { dispatch(onFieldChange('name', text))}}/>
-          <ProfileField iconName="mail" value={edit ? fields.email : user.email}
-              editable={editable} onChangeText={(text) => { dispatch(onFieldChange('email', text))}}/>
-          <ProfileField iconName="cake" isBirthday={true} value={edit ? fields.birthday : user.birthday}
-              editable={editable} onDateChange={(date) => {dispatch(onFieldChange('birthday', date))}}/>
-          <ProfileField iconName="phone" value={user.phoneNumber}/>
+          <ProfileField
+            iconName="person"
+            value={edit ? fields.name : user.name}
+            editable={editable}
+            onChangeText={text => {
+              dispatch(onFieldChange('name', text))
+            }}
+          />
+          <ProfileField
+            iconName="mail"
+            value={edit ? fields.email : user.email}
+            editable={editable}
+            onChangeText={text => {
+              dispatch(onFieldChange('email', text))
+            }}
+          />
+          <ProfileField
+            iconName="cake"
+            isBirthday={true}
+            value={edit ? fields.birthday : user.birthday}
+            editable={editable}
+            onDateChange={date => {
+              dispatch(onFieldChange('birthday', date))
+            }}
+          />
+          <ProfileField iconName="phone" value={user.phoneNumber} />
         </View>
 
-        {showVersionNumber ? 
+        {showVersionNumber ? (
           <DjiniText style={styles.versionText}>{version}</DjiniText>
-          : undefined
-        }
+        ) : (
+          undefined
+        )}
+        {showLogoutButton ? (
+          <DjiniButton
+            style={styles.logoutButton}
+            onPress={() => dispatch(logout())}
+            caption="Logout"
+          />
+        ) : (
+          undefined
+        )}
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   appBar: {
     backgroundColor: 'rgb(240, 240, 240)'
@@ -71,7 +122,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: 'white',
     fontStyle: 'italic',
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   fields: {
     marginTop: 40,
@@ -82,13 +133,14 @@ const styles = StyleSheet.create({
   },
   versionText: {
     position: 'absolute',
-    bottom: 0, right: 0,
+    bottom: 0,
+    right: 0,
     fontSize: 11
   }
-});
+})
 
 function select(state) {
-  return {...state.profile.toJS(), user: state.global.currentUser}
+  return { ...state.profile.toJS(), user: state.global.currentUser }
 }
 
-export default connect(select)(Profile);
+export default connect(select)(Profile)
